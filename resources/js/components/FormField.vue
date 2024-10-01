ce26b387e8d37d2cca32380a13d42d305081dc46
Keko-94 <62820283+Keko-94@users.noreply.github.com>
2024-10-01 12:19:43 +0300

    Update FormField.vue

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,6 +1,6 @@
 <template>
   <DefaultField
-    :field="field"
+    :field="currentField"
     :errors="errors"
     :show-help-text="showHelpText"
     :full-width-content="fullWidthContent"
@@ -9,34 +9,35 @@
       <Vue3Signature :sigOption="{penColor: color, backgroundColor: bgColor}" :w="width" :h="height"
                      :disabled="disabled"  ref="signature" class="form-input-bordered rounded" @begin="onBegin" @end="onEnd" />
 
-  <div class="flex flex-row align-middle md:items-center justify-center md:justify-end space-x-1 space-y-0 md:space-x-3 my-2">
-  <button type="button"
-          class="shadow bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900 cursor-pointer rounded text-sm inline-block font-bold w-1/2 md:w-32 h-9 px-3"
-          @click="undo">
+      <div class="flex flex-row align-middle md:items-center justify-center md:justify-end space-x-1 space-y-0 md:space-x-3 my-2">
+        <button type="button"
+                class="shadow bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900 cursor-pointer rounded text-sm inline-block font-bold w-1/2 md:w-32 h-9 px-3"
+                @click="undo">
           <component
             :is="`heroicons-outline-reply`"
             height="18"
             width="18"
             class="inline"
           /> {{__('novaSignature.undo') }}
-  </button>
-  <button type="button"
-          class="shadow bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900 cursor-pointer rounded text-sm inline-block font-bold w-1/2 md:w-32 h-9 px-3"
-          @click="clear">
+        </button>
+        <button type="button"
+                class="shadow bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900 cursor-pointer rounded text-sm inline-block font-bold w-1/2 md:w-32 h-9 px-3"
+                @click="clear">
           <component
             :is="`heroicons-outline-x`"
             height="18"
             width="18"
             class="inline"
           /> {{__('novaSignature.clear') }}
-  </button>
-  </div>
+        </button>
+      </div>
 
-      <textarea 
-        :id="field.attribute"
+      <textarea
+        :id="currentField.attribute"
         type="text"
         class="hidden"
-        v-model="value" rows="10"
+        v-model="value"
+        rows="10"
       >
         {{value}}
       </textarea>
@@ -46,21 +47,33 @@
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
+import { DependentFormField, HandlesValidationErrors } from 'laravel-nova'
 
 export default {
-  mixins: [FormField, HandlesValidationErrors],
+  mixins: [DependentFormField, HandlesValidationErrors],
   props: ['resourceName', 'resourceId', 'field'],
-  data() {
-      return {
-          height: '300px',
-          width: '100%',
-          color: 'black',
-          bgColor: 'white',
-          disabled: false,
-          format: 'image/png'
+
+  watch: {
+    currentlyIsVisible(current, previous) {
+      if (current === true && previous === false) {
+        this.$nextTick(() => this.handleShow())
+      } else if (current === false && previous === true) {
+        console.log('hide')
       }
+    },
   },
+
+  data() {
+    return {
+      height: '300px',
+      width: '100%',
+      color: 'black',
+      bgColor: 'white',
+      disabled: false,
+      format: 'image/png'
+    }
+  },
+
   beforeMount() {
     this.width = this.field.width || this.width;
     this.height = this.field.height || this.height;
@@ -68,30 +81,37 @@ export default {
     this.bgColor = this.field.bgColor || this.bgColor;
     this.format = this.field.format || this.format;
   },
+
   mounted() {
-        this.$nextTick().then(() => {
-            if (this.field.value) {
-                this.value = this.field.value;
-                this.$refs.signature.fromDataURL(this.value);
-            }
-        });
-    },
+    this.$nextTick().then(() => {
+      if (this.currentField.value && this.currentField.visible) {
+        this.value = this.currentField.value;
+        this.$refs.signature?.fromDataURL(this.value);
+      }
+    });
+  },
+
   methods: {
+    handleShow() {
+      this.value = this.currentField.value;
+      this.$refs.signature.fromDataURL(this.value);
+    },
+
     clear() {
-        this.$refs.signature.clear()
-        this.value = null
+      this.$refs.signature.clear()
+      this.value = null
     },
 
     undo() {
-        this.$refs.signature.undo()
-        this.save();
+      this.$refs.signature.undo()
+      this.save();
     },
 
     onBegin() {
     },
 
     onEnd() {
-       this.save();
+      this.save();
     },
 
     save() {
@@ -101,19 +121,19 @@ export default {
         this.value = this.$refs.signature.save(this.format);
       }
     },
-   
+
     /*
      * Set the initial, internal value for the field.
      */
     setInitialValue() {
-      this.value = this.field.value || '';
+      this.value = this.currentField.value || '';
     },
 
     /**
      * Fill the given FormData object with the field's internal value.
      */
     fill(formData) {
-      formData.append(this.fieldAttribute, this.value || '')
+      this.fillIfVisible(formData, this.fieldAttribute, this.value || '')
     },
   },
 }
